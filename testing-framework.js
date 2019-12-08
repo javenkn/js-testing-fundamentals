1501119da3f8a3cb8accec64f1b4414abb69d39c
javenkn <javenkn7@gmail.com>
2019-12-07 20:30:15 -1000

    Adds test functionality for encapsulating and isolating the tests

---
+++ testing-framework.js
@@ -0,0 +1,37 @@
+/**
+ * An testing framework that encapsulates and isolates the tests.
+ */
+
+const { add, subtract } = require("./math");
+
+test("sum adds numbers", () => {
+  const result = add(7, 3);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test("subtract subtracts numbers", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✅ ${title}`);
+  } catch (error) {
+    console.log(`❌ ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    }
+  };
+}
